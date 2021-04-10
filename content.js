6f4959a2c5f707f066cadd878e028d28aa4213d9
ikaken <user@ikaken.com>
2021-04-11 01:23:59 +0900

    使っていない変数削除、使っていない処理削除、gsapをgsap.minへ

--- content.js
+++ content.js
@@ -1,47 +1,34 @@
-
 var interval;
-//let count = 0;
 let msgcount = 0;
 let msgtext = "";
 
 console.log("content.js ver 05");
 
+//popup.jsからのメッセージで発火
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
-    // debugger;
 
     console.log("var 52");
 
-    //チャットメッセージ取得処理（定期実行）
     if (request.method === "openCommentPage") {
-
       console.log("setInterval 01");
 
       clearInterval(interval);
+      //チャットメッセージ取得処理（定期実行）
       interval = setInterval(() => {
         const query = document.querySelectorAll('.chat-message-text-content');  //メッセージの履歴取得（複数）
         console.log(query.length);    //メッセージの数
         if (msgcount != query.length) {   //新しいメッセージが入ったら
-          for (let e of query) {
-            console.log(e.textContent + e.id);
-          }
-          msgtext = query[query.length - 1].textContent;    //メッセージテキストを生成（最後のメッセージのテキストを取得）
+          msgtext = query[query.length - 1].textContent;    //最後のメッセージのテキストを取得
           msgcount = query.length;    //メッセージ数（カウント）を更新
-
-          //          chrome.runtime.sendMessage({ method: "sndmsg", text: msgtext });  //バックグランドページへメッセージ送信
-
-          //データを保存
+          //データをstorageへ保存
           chrome.storage.local.set({ sndmsg: msgtext }, function () {
             console.log('storage local set ver 02 => ' + msgtext);
           });
-
         }
-
-
-      }, 1000);
+      }, 1000); //チャット取得間隔（1000ミリ秒）
 
     }
-
 
   }
 );
--- popup.js
+++ popup.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+  //ボタンが押された処理
   var commentpage = document.getElementById('openCommentPage');
   commentpage.addEventListener('click', () => {
-
+    //アクティブタブのコンテンツスクリプトへメッセージを送る
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(tabs[0].id, { method: "openCommentPage" });
-      console.log(tabs[0].id);
-
+      //グリーンバック用のページを開く
       chrome.tabs.create({ url: "https://test.ikaken.com/content2.html" }, (tab) => {
       });
-
     });
   }, false);
 
